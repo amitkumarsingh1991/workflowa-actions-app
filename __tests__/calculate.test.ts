1c9e7daa7a38a76911808fd186d14fe7fd650f24
amitvikashsingh <amitkumar.821105@gmail.com>
2023-08-27 23:05:30 +0400

    Init

---
+++ __tests__/calculate.test.ts
@@ -0,0 +1,8 @@
+import { fireEvent, render, screen, waitFor, act} from "@testing-library/react";
+
+describe("Todo App", () => {
+    it("Add two number", () => {
+       const result = "21";
+        expect(result).toEqual("21");
+    });
+  });
